44409cce47288abb28358c9151715b74a18cf8f3
VitalSoroka <vitalijsoroka40@gmail.com>
2024-11-02 18:02:49 +0300

    init backend service

---
+++ callboard-service/src/main/java/com/application/callboard/CallboardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.application.callboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallboardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallboardServiceApplication.class, args);
+	}
+
+}
---
+++ callboard-service/src/test/java/com/application/callboard/CallboardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.callboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallboardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
