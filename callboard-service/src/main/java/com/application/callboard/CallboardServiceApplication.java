b5626205383e70a7f987e283debc62a1bb62b2a5
VitalSoroka <vitalijsoroka40@gmail.com>
2024-11-02 22:38:47 +0300

    Disable datasource autoconfiguration

--- callboard-service/src/main/java/com/application/callboard/CallboardServiceApplication.java
+++ callboard-service/src/main/java/com/application/callboard/CallboardServiceApplication.java
@@ -2,8 +2,11 @@ package com.application.callboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@ComponentScan
 public class CallboardServiceApplication {
 
 	public static void main(String[] args) {
